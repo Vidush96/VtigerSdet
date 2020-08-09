8e8fa8226eb3a688b1997ebc38503e9693016fa0
hp <hp@Vidush-M-Aric>
2020-08-09 22:32:22 +0530

    Added POM Home_page class Modified CreateOrgAndDeleteOrgTest and CreateOrgTest by getting element from home_page for navigating into organization page

--- src/main/java/com/vtigercrm/objectrepositoryLib/Home_Page.java
+++ src/main/java/com/vtigercrm/objectrepositoryLib/Home_Page.java
@@ -1,6 +1,29 @@
 package com.vtigercrm.objectrepositoryLib;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class Home_Page 
 {
+	@FindBy(linkText = "Organizations")
+	private WebElement Orglink;
+	
+	@FindBy(linkText = "Contacts")
+	private WebElement Contactlink;
+	
+	public Home_Page(WebDriver driver) 
+	{
+		PageFactory.initElements(driver, this);
+	}
 
+	public WebElement getOrglink() {
+		return Orglink;
+	}
+
+	public WebElement getContactlink() {
+		return Contactlink;
+	}
+	
 }
