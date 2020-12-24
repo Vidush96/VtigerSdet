ee060f872f07fb5b879956357b86afb52f172d89
Malathi M <Malathi M@DESKTOP-8FGMNFR>
2020-12-24 13:31:56 +0530

    Have added the description

--- src/main/java/com/vtigercrm/genericutils/Base_Class.java
+++ src/main/java/com/vtigercrm/genericutils/Base_Class.java
@@ -25,7 +25,7 @@ import com.vtigercrm.objectrepositoryLib.Vtiger_Login_Page;
  */
 /**
  * 
- * This class consists of details related to DB, Browsers, Cross browsers
+ * This class consists of details related to DB, Browsers
  *
  */
 public class Base_Class 
